a32823cb81854a0a1c32184dab6bd596a4a63f44
Victor Huerta <vhuertahnz@gmail.com>
2017-04-25 23:46:52 -0500

    Api just for test

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,9 @@
 const isDev = process.env.NODE_ENV !== 'production';
-const endpoint = isDev
-  ? 'http://localhost:3001'
-  : 'https://khe-be-api.herokuapp.com';
+// const endpoint = isDev
+//   ? 'http://localhost:3001'
+//   : 'https://khe-be-api.herokuapp.com';
+
+const endpoint = 'https://khe-be-api.herokuapp.com';
 
 export default {
   api: {
